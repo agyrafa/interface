eb33a3b45de9a2ae4bf38e7c152bafe0941936d5
agyrafa <agyrafa@gmail.com>
2019-04-16 17:32:55 -0300

    add new alerts

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -2,14 +2,18 @@
  * Main App Controller for the AngularJS Material Starter App
  * @param UsersDataService
  * @param $mdSidenav
+ * @param $mdDialog
  * @constructor
  */
-function AppController(UsersDataService, $mdSidenav) {
+function AppController(UsersDataService, $mdSidenav, $mdDialog) {
   var self = this;
 
   self.selected     = null;
   self.users        = [ ];
   self.selectUser   = selectUser;
+  self.showAlert    = showAlert;
+  self.newItem      = newItem;
+  self.selectItem   = selectItem;
   self.toggleList   = toggleUsersList;
 
   // Load all registered users
@@ -18,7 +22,7 @@ function AppController(UsersDataService, $mdSidenav) {
         .loadAllUsers()
         .then( function( users ) {
           self.users    = [].concat(users);
-          self.selected = users[0];
+          // self.selected = users[0];
         });
 
   // *********************************
@@ -38,7 +42,51 @@ function AppController(UsersDataService, $mdSidenav) {
    */
   function selectUser ( user ) {
     self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+
+    self.showAlert();
+  } 
+
+  function showAlert() {
+    alert = $mdDialog.alert({
+      title: 'Alerta',
+      textContent: 'Usuário selecionado',
+      ok: 'Fechar'
+    });
+
+    $mdDialog
+      .show( alert )
+      .finally(function() {
+        alert = undefined;
+      });
+  }
+
+  function newItem(){
+    alert = $mdDialog.alert({
+      title: 'Alerta',
+      textContent: 'Novo atendimento',
+      ok: 'Fechar'
+    });
+
+    $mdDialog
+      .show( alert )
+      .finally(function() {
+        alert = undefined;
+      });
+  }
+
+  function selectItem(){
+    alert = $mdDialog.alert({
+      title: 'Alerta',
+      textContent: 'Selecione o atendimento',
+      ok: 'Fechar'
+    });
+
+    $mdDialog
+      .show( alert )
+      .finally(function() {
+        alert = undefined;
+      });
   }
 }
 
-export default [ 'UsersDataService', '$mdSidenav', AppController ];
+export default [ 'UsersDataService', '$mdSidenav', '$mdDialog', AppController ];
